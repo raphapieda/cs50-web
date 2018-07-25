86533d40fb60354b334bf1afec2cd1ad7bdb0f62
kleberyokota <kleber_yokota@hotmail.com>
2018-07-25 19:16:00 -0300

    Create Template to create account

--- project1/application.py
+++ project1/application.py
@@ -1,26 +1,33 @@
 import os
 
-from flask import Flask, session
+from flask import Flask, session, render_template, request
+
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 
 app = Flask(__name__)
+# __name__
+# # Check for environment variable
+# if not os.getenv("DATABASE_URL"):
+#     raise RuntimeError("DATABASE_URL is not set")
+#
+# # Configure session to use filesystem
+# app.config["SESSION_PERMANENT"] = False
+# app.config["SESSION_TYPE"] = "filesystem"
+# Session(app)
+#
+# # Set up database
+# engine = create_engine(os.getenv("DATABASE_URL"))
+# db = scoped_session(sessionmaker(bind=engine))
 
-# Check for environment variable
-if not os.getenv("DATABASE_URL"):
-    raise RuntimeError("DATABASE_URL is not set")
 
-# Configure session to use filesystem
-app.config["SESSION_PERMANENT"] = False
-app.config["SESSION_TYPE"] = "filesystem"
-Session(app)
-
-# Set up database
-engine = create_engine(os.getenv("DATABASE_URL"))
-db = scoped_session(sessionmaker(bind=engine))
-
-
-@app.route("/")
+@app.route("/", methods =['POST','GET'])
 def index():
-    return "Project 1: TODO"
+    if request.method == 'POST':
+        username = request.form['uname']
+        psw = request.form['psw']
+        data = {'username': username, 'psw': psw}
+        return render_template('create_login.html', data = data)
+    else:
+        return render_template('index.html')
